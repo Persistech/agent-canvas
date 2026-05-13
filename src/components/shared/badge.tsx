674959722f2710974f1ab3d8ed42798ec12dcb9e
VascoSch92 <vasco.schiavo@protonmail.com>
2026-05-13 13:18:00 +0200

    fix(profiles): use brand badge for active profile indicator

--- src/components/shared/badge.tsx
+++ src/components/shared/badge.tsx
@@ -1,12 +1,11 @@
 import { cn } from "#/utils/utils";
 
-interface BrandBadgeProps {
-  className?: string;
-}
+type BrandBadgeProps = React.HTMLAttributes<HTMLSpanElement>;
 
 export function BrandBadge({
   children,
   className,
+  ...rest
 }: React.PropsWithChildren<BrandBadgeProps>) {
   return (
     <span
@@ -14,6 +13,7 @@ export function BrandBadge({
         "text-sm leading-4 text-[#0D0F11] font-semibold tracking-tighter bg-primary p-1 rounded-full",
         className,
       )}
+      {...rest}
     >
       {children}
     </span>
